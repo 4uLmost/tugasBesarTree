1a21449d9969375f49529d2b2697c8f2fa8a6bcb
Muhammad Dhiyaul <97652456+4uLmost@users.noreply.github.com>
2025-12-27 11:51:10 +0700

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <limits> // Untuk numeric_limits
+#include <limits>
 using namespace std;
 
 struct infotype {
@@ -19,26 +19,21 @@ struct Node {
     adrNode nextSibling;
 };
 
-// --- PRIMITIF TREE ---
 void createNode(infotype X, adrNode &P);
 void insertNode(adrNode Parent, adrNode P);
 adrNode findNode(adrNode root, string name);
 void deleteNode(adrNode &root, string name);
 void deleteTree(adrNode P);
-
-// --- TRAVERSAL ---
 void printTree(adrNode root, int depth = 0);
 void printPreOrder(adrNode root);
 void printInOrder(adrNode root);
 void printPostOrder(adrNode root);
-
-// --- FITUR UNIK ---
 int countTotalAssetStock(adrNode root);
 void findMaxStock(adrNode root, adrNode &maxNode);
 int countNodes(adrNode root);
 
-// --- FUNGSI BANTUAN (UTILITY) ---
 void clearBuffer();
 void waitForEnter();
 
 #endif
+
